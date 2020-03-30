abc43c772728229ef1be57713d8e5e074cbf656b
Harry Cogswell <harry.cogswell@gmail.com>
2020-03-30 10:37:32 -0700

    queries work. need to be refactored to single query

--- Lab07-LINQ/Lab07-LINQ/Program.cs
+++ Lab07-LINQ/Lab07-LINQ/Program.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using Lab07_LINQ.Classes;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Linq;
 
 namespace Lab07_LINQ
 {
@@ -7,10 +13,83 @@ namespace Lab07_LINQ
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            QueryData();
         }
-        public void ReadData()
-        {
 
+        static RootObject ReadData(string path)
+        {
+            string jsonData = File.ReadAllText(path);
+            RootObject deserializedRoot = JsonConvert.DeserializeObject<RootObject>(jsonData);
+            return deserializedRoot;
+        }
+
+        static void QueryData()
+        {
+            RootObject hoodCollection = ReadData("../../../data.json");
+
+            QueryAll(hoodCollection);
+
+            Console.WriteLine("Press enter to see only named neighbourhoods.");
+            Console.ReadLine();
+            QueryNamed(hoodCollection);
+
+            Console.WriteLine("Press enter to see all neighbourhoods without duplicates.");
+            Console.ReadLine();
+            QueryNoDupes(hoodCollection);
+        }
+
+        static void QueryAll(RootObject collection)
+        {
+            Console.Clear();
+            Console.WriteLine("These are all the new york neighborhoods in my database: \n(press Enter)");
+            Console.ReadLine();
+            var query = from item in collection.features
+                        select item.properties.neighborhood;
+
+            int count = 0;
+            foreach (string hood in query)
+            {
+                count++;
+                Console.WriteLine(hood);
+            }
+            Console.WriteLine($"Total Neighborhoods: {count}");
+        }
+
+        static void QueryNamed(RootObject collection)
+        {
+            Console.Clear();
+            Console.WriteLine("These are all the named new york neighborhoods: \n(press Enter)");
+            Console.ReadLine();
+            var query = from item in collection.features
+                        where item.properties.neighborhood != ""
+                        select item.properties.neighborhood;
+
+            int count = 0;
+            foreach (string hood in query)
+            {
+                count++;
+                Console.WriteLine(hood);
+            }
+            Console.WriteLine($"Total Neighborhoods: {count}");
+        }
+
+        static void QueryNoDupes(RootObject collection)
+        {
+            Console.Clear();
+            Console.WriteLine("These are all the new york neighborhoods, without duplicates: \n(press Enter)");
+            Console.ReadLine();
+
+            var query = from item in collection.features
+                        where item.properties.neighborhood != ""
+                        select item.properties.neighborhood;
+
+            int count = 0;
+            foreach (string hood in query.Distinct())
+            {
+                count++;
+                Console.WriteLine(hood);
+            }
+            Console.WriteLine($"Total Neighborhoods: {count}");
         }
     }
 }
