7655fd86830384ff7697c2c51763caa23397cdeb
Harry Cogswell <harry.cogswell@gmail.com>
2020-03-29 19:57:08 -0700

    initial commit, json included

---
+++ Lab07-LINQ/Lab07-LINQ/Classes/Neighborhood.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab07_LINQ.Classes
+{
+    class Neighborhood
+    {
+        public string Name { get; set; }
+        public float Lat { get; set; }
+        public float Lon { get; set; }
+        
+        public Neighborhood(float[] coords, string name = "unnamed")
+        {
+            Name = name;
+            Lat = coords[0];
+            Lon = coords[1];
+        }
+    }
+}
---
+++ Lab07-LINQ/Lab07-LINQ/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Lab07_LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public void ReadData()
+        {
+
+        }
+    }
+}
